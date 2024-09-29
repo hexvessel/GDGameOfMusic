c612aa025c4308460e221eb89a8561f463c1e677
hexvessel <dho1@hi.is>
2024-09-29 23:05:20 +0000

    first commit

---
+++ scenes/Tiles.cs
@@ -0,0 +1,148 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class Tiles : Node2D
+{
+    TileMapLayer map;
+    HSlider slider;
+    Timer timer;
+    AudioStreamPlayer audio;
+    Vector2I TileId = new Vector2I(0, 0);
+    Vector2I size = new Vector2I(73, 41);
+    List<Vector2I> live = new List<Vector2I> { };
+    List<Vector2I> audioMap = new List<Vector2I> { };
+    List<string> audioPaths = new List<string> {
+         "res://audio/0.wav",
+         "res://audio/2.wav",
+         "res://audio/5.wav",
+         "res://audio/7.wav",
+         "res://audio/9.wav",
+         "res://audio/12.wav",
+         "res://audio/14.wav",
+         "res://audio/17.wav",
+         "res://audio/19.wav",
+         "res://audio/21.wav",
+         "res://audio/24.wav",
+ };
+
+    public override void _Ready()
+    {
+        map = GetNode<TileMapLayer>("TileMapLayer");
+        slider = GetNode<HSlider>("HSlider");
+        timer = GetNode<Timer>("Timer");
+        audio = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+        audioMap = GenerateNoteNodes(5, 5);
+    }
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("mbleft"))
+        {
+            Vector2I coords = map.LocalToMap(GetGlobalMousePosition());
+            if (coords.Y < 36)
+            {
+                map.SetCell(map.LocalToMap(GetGlobalMousePosition()), 0, TileId);
+            }
+        }
+    }
+    public void Step()
+    {
+        live.Clear();
+        for (int i = 0; i < size.X; i++)
+        {
+            for (int j = 0; j < size.Y; j++)
+            {
+                int cell = map.GetCellSourceId(new Vector2I(i, j));
+                int n = GetNeighbors(i, j);
+                if (cell > -1)
+                {
+                    if (n == 2 || n == 3)
+                    {
+                        live.Add(new Vector2I(i, j));
+                        int indexOfAudioFile = audioMap.IndexOf(new Vector2I(i, j));
+                        if (indexOfAudioFile >= 0)
+                        {
+                            audio.Stream = ResourceLoader.Load<AudioStream>(audioPaths[indexOfAudioFile % 11]);
+                            audio.Play();
+                        }
+                    }
+                }
+                else if (n == 3)
+                {
+                    live.Add(new Vector2I(i, j));
+                }
+            }
+        }
+        map.Clear();
+        foreach (var cell in live)
+        {
+            TileId = audioMap.Contains(cell) ? new Vector2I(1, 0) : new Vector2I(0, 0);
+            map.SetCell(cell, 0, TileId);
+        }
+
+    }
+    public int GetNeighbors(int x, int y)
+    {
+        int n = 0;
+        for (int i = x - 1; i < x + 2; i++)
+        {
+            for (int j = y - 1; j < y + 2; j++)
+            {
+                if (!(i == x && j == y))
+                {
+                    if (map.GetCellSourceId(new Vector2I(i, j)) > -1)
+                    {
+                        n++;
+                    }
+                }
+            }
+        }
+        return n;
+    }
+    public void ValueChanged(float Value)
+    {
+        if (Value == 0)
+        {
+            timer.Stop();
+        }
+        else if (timer.IsStopped() && Value > 0)
+        {
+            timer.Start();
+        }
+        timer.WaitTime = Value;
+    }
+    public void ButtonPressed()
+    {
+        map.Clear();
+    }
+    public void ShufflePressed()
+    {
+        audioPaths = Shuffle(audioPaths);
+    }
+    public List<Vector2I> GenerateNoteNodes(int xStep, int yStep)
+    {
+        List<Vector2I> list = new List<Vector2I>();
+        for (int i = xStep; i < size.X; i += xStep)
+        {
+            for (int j = yStep; j < size.Y; j += yStep)
+            {
+                list.Add(new Vector2I(i, j));
+            }
+        }
+        return list;
+    }
+    public List<string> Shuffle(List<string> list)
+    {
+        var count = list.Count;
+        var last = count - 1;
+        for (int i = 0; i < last; ++i)
+        {
+            var r = new Random().Next(i, count);
+            var tmp = list[i];
+            list[i] = list[r];
+            list[r] = tmp;
+        }
+        return list;
+    }
+}
